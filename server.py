fcb56c95e490fc3331887fdf0ee24ea543766c20
Sanjit <randomguyfromindia@gmail.com>
2021-11-15 10:35:58 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+
+app = Flask(__name__)
+print(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+	return render_template(page_name)
+
+# # def write_to_csv(data):
+# # 	with open('database.csv', mode='a') as database2:
+# # 		email = data["email"]
+# # 		subject = data["subject"]
+# # 		message = data["message"]
+# # 		file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+	with open('database.csv', newline='', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+# def write_to_csv(data):
+#     fields = ['Email', 'Subject', 'Message']
+#     # will have to import os to use this check
+#     if not os.path.isfile('database.csv'):
+#         with open('database.csv', 'w', newline='') as csv_file:
+#             writer = csv.DictWriter(csv_file, fieldnames=fields)
+#             writer.writeheader()
+#             writer.writerow(data)
+#     else:
+#         with open('database.csv', 'a', newline='') as csv_file:
+#             writer = csv.DictWriter(csv_file, fieldnames=fields)
+#             writer.writerow(data)
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return redirect('thankyou.html')
+		except:
+			return 'did not save to database'
+	else:
+		return 'something went wrong. Try again!'
